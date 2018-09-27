d364e9e7b55a087935ac8226c54a11b8c3229d5c
mihailov83 <42243106+mihailov83@users.noreply.github.com>
2018-09-28 00:27:47 +0200

    Add var "a ", "h" & "return" in else.

--- scripts.js
+++ scripts.js
@@ -1,16 +1,20 @@
 // scripts.js
 
+var a;
+var h;
+
 function getTriangleArea(a, h) {
     if (a > 0 && h > 0) {
         return a * h / 2;
     } else {
+        return 'nieprawidlowe dane';
         console.log('nieprawidlowe dane');
     }
 }
 
-var triangle1Area = getTriangleArea(3, 12),
-    triangle2Area = getTriangleArea(6, 10),
-    triangle3Area = getTriangleArea(20, 13);
+var triangle1Area = getTriangleArea(3, -3),
+    triangle2Area = getTriangleArea(2, 4),
+    triangle3Area = getTriangleArea(20, 5);
 
 console.log(triangle1Area);
 console.log(triangle2Area);
